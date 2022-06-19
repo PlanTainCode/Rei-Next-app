68751edf68f02ddc066d67fe8ed1462e9c8fdb46
PlanTainCode <plancode14@gmail.com>
2022-06-19 10:53:59 +0300

    main

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
+import { ThemeProvider } from '@mui/system'
+import '../styles/globals.scss'
+import { theme } from '../theme'
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
